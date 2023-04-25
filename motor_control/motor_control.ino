63a48c84a8940d50fca408d9bd10e11c2cda2053
Ian Davison <idavison157@gmail.com>
2023-04-25 14:56:02 -0600

    First commit

---
+++ motor_control/motor_control.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+
+void setup() {
+  AFMS.begin();
+  myMotor->setSpeed(150);
+  myMotor2->setSpeed(150);
+}
+
+void loop() {
+  right();
+  delay(1000);
+  left();
+  delay(1000);
+}
+
+void right() {
+  myMotor->run(FORWARD);
+  myMotor2->run(BACKWARD);
+}
+
+void left() {
+  myMotor2->run(FORWARD);
+  myMotor->run(BACKWARD);
+}
