00db2deb011d2de4bd9baaf8a0b5c21f0d26b3a1
Ian Davison <idavison157@gmail.com>
2023-04-26 15:58:02 -0600

    Sensor and bluetooth tests

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -8,15 +8,22 @@ Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
 
 void setup() {
   AFMS.begin();
+  Serial.begin(9600);
   myMotor->setSpeed(150);
   myMotor2->setSpeed(150);
 }
 
 void loop() {
-  right();
-  delay(1000);
-  left();
-  delay(1000);
+
+  char cmd = Serial.read();
+  if(cmd == 's'){
+    myMotor->run(FORWARD);
+    myMotor2->run(FORWARD);
+    delay(5000);
+    myMotor->setSpeed(0);
+    myMotor2->setSpeed(0);
+  }
+
 }
 
 void right() {
