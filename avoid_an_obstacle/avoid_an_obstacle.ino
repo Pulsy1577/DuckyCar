f5b600d3b666c91861fc9b1cc095bbdde8b79885
Ian Davison <idavison157@gmail.com>
2023-05-01 17:58:44 -0600

    turnRight90 added

--- avoid_an_obstacle/avoid_an_obstacle.ino
+++ avoid_an_obstacle/avoid_an_obstacle.ino
@@ -29,6 +29,7 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
 float d1_meas = 100;
+float d2_meas = 0;
 float heading = 0;
 
 float lastDistVal[] = {0,0,0};
@@ -47,30 +48,29 @@ void setup() {
 }
 
 void loop() {
+
+  ICM_20948_I2C *sensor = &myICM;
+  float z = sensor->gyrZ(); 
+
+  float delta_s = (millis() - sensor_timer);
+  heading += z * (delta_s/1000);
+  sensor_timer = millis();
+
   myMotor->run(FORWARD);
   myMotor2->run(FORWARD);
-
-  // put your main code here, to run repeatedly:
-  // Read ultrasonic sensor measurements
-
-  //printSensorData(d1_meas);
-  //Serial.println((d1_meas));
-  //obstacle avoidance
-  if ((d1_meas) < 6){
-    turnRight();
-  }
-  if (heading >= 90) {
-    myMotor->run(FORWARD);
-    myMotor2->run(FORWARD);
-  }
   
-
   d1_meas = readUltrasonic(trigPin1, echoPin1);
   float d2_meas = readUltrasonic(trigPin2, echoPin2);
   float d3_meas = readUltrasonic(trigPin3, echoPin3);
 
+  if ((d1_meas) < 6){
+    Serial.println(d1_meas);
+    delay(2000);
+    turnLeft90();
+  }
+
   getIMU();
-  
+
   // send the sensor data over i2c as an array of bytes
   uint8_t sensor_data[] = {static_cast<uint8_t>(d1_meas), static_cast<uint8_t>(d2_meas), static_cast<uint8_t>(d3_meas)};
   Wire.beginTransmission(8); // transmit to device with address 8
@@ -79,16 +79,6 @@ void loop() {
   //z << d1_meas, d2_meas, d3_meas;
   //delay(2000);
 
-  ICM_20948_I2C *sensor = &myICM;
-  float z = sensor->gyrZ(); 
-
-  float delta_s = (millis() - sensor_timer);
-
-  heading += z * (delta_s/1000);
-  sensor_timer = millis();
-  Serial.println(heading);
-
-  
   lastDistVal[0] = d1_meas;
   lastDistVal[1] = d2_meas;
   lastDistVal[2] = d3_meas;
@@ -167,6 +157,40 @@ void turnRight(){
   Serial.println("Turning Right");
   myMotor->run(BACKWARD);
   myMotor2->run(FORWARD);
+}
+
+void turnLeft(){
+  Serial.println("Turning Left");
+  myMotor->run(FORWARD);
+  myMotor2->run(BACKWARD);
+}
+
+int turnLeft90(){
+
+  float target = heading - 90;
+
+  myMotor->run(FORWARD);
+  myMotor2->run(BACKWARD);
+
+  while (heading + 10 > target && heading - 10 > target){
+    ICM_20948_I2C *sensor = &myICM;
+    float z = sensor->gyrZ(); 
+    float delta_s = (millis() - sensor_timer);
+    Serial.print("Delt s: ");
+    Serial.println(delta_s);    
+    heading += z * (delta_s/1000);
+    sensor_timer = millis();
+    Serial.println("Turning Left");
+    Serial.print("Heading: ");
+    Serial.println(heading);
+    Serial.print("Target: ");
+    Serial.println(target);
+    delay(100);
+  }
+  myMotor->run(FORWARD);
+  myMotor2->run(FORWARD);
+  return 0;
+  
 }
 
 void avoidRight(){
