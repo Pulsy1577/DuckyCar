2a3c931f335a20c3dc887fdbd90f56d044e9f9a1
J526Tx <jtboyle.11@gmail.com>
2023-05-01 16:02:37 -0600

    new i2c
    new i2c

--- i2c/i2c.ino
+++ i2c/i2c.ino
@@ -1,6 +1,6 @@
 // These must be defined before including TinyEKF.h
-#define Nsta 2     // Two state values: pressure, temperature
-#define Mobs 3     // Three measurements: baro pressure, baro temperature, LM35 temperature
+#define Nsta 5     // 5 state values: position in 3d (x, y, z), velocity in 3d (x, y,z), acceleration in 3d, orientation in 3d (pitch, roll, yaw), sensor bias
+#define Mobs 4     // 4 measurements: 3 distance measurements, one measurement of acceleration and gryoscope data
 
 #define LM35_PIN 0
 #include <Wire.h> // for I2C communication
@@ -19,10 +19,117 @@ const int trigPin2 = 5;
 const int echoPin2 = 4;
 const int trigPin3 = 7;
 const int echoPin3 = 6;
-
+float x, y, z, gx, gy, gz;
+//double z[Mobs];
 ICM_20948_I2C myICM;
+class Filter : public TinyEKF {
+    private:
+      double dt; // time step
+      double P[Nsta][Nsta]; // state covariance matrix
+    public:
+        Filter()
+        {            
+            // We set the initial state to zero
+            this->x[0] = 0; // position x
+            this->x[1] = 0; // position y
+            this->x[2] = 0; // position z
+            this->x[3] = 0; // velocity x
+            this->x[4] = 0; // velocity y
+            this->x[5] = 0; // velocity z
+            this->x[6] = 0; // acceleration x
+            this->x[7] = 0; // acceleration y
+            this->x[8] = 0; // acceleration z
+            this->x[9] = 0; // orientation pitch
+            this->x[10] = 0; // orientation roll
+            this->x[11] = 0; // orientation yaw
+            this->x[12] = 0; // sensor bias
 
+            // We set the initial state covariance to a high value to indicate high uncertainty
+            for (int i = 0; i < Nsta; i++) {
+              for (int j = 0; j < Nsta; j++) {
+                if (i == j) {
+                  this->P[i][j] = 100.0;
+                } else {
+                  this->P[i][j] = 0.0;
+                }
+              }
+            }
 
+            // We set the process noise to a small value to indicate low uncertainty
+            this->setQ(0, 0, 0.01);
+            this->setQ(1, 1, 0.01);
+            this->setQ(2, 2, 0.01);
+            this->setQ(3, 3, 0.01);
+            this->setQ(4, 4, 0.01);
+            this->setQ(5, 5, 0.01);
+            this->setQ(6, 6, 0.001);
+            this->setQ(7, 7, 0.001);
+            this->setQ(8, 8, 0.001);
+            this->setQ(9, 9, 0.001);
+            this->setQ(10, 10, 0.001);
+            this->setQ(11, 11, 0.001);
+            this->setQ(12, 12, 0.001);
+
+            // Same for measurement noise
+            this->setR(0, 0, 0.1);
+            this->setR(1, 1, 0.1);
+            this->setR(2, 2, 0.1);
+            this->setR(3, 3, 0.1);
+        }
+    protected:
+    double z[Mobs];
+    void model(double fx[Nsta], double F[Nsta][Nsta], double hx[Mobs], double H[Mobs][Nsta])
+    {
+        // Process model is f(x) = x
+        fx[0] = this->x[0] + this->x[3] * this->dt; // x position
+        fx[1] = this->x[1] + this->x[4] * this->dt; // y position
+        fx[2] = this->x[2] + this->x[5] * this->dt; // z position
+        fx[3] = this->x[3] + this->x[6] * this->dt; // x velocity
+        fx[4] = this->x[4] + this->x[7] * this->dt; // y velocity
+        fx[5] = this->x[5] + this->x[8] * this->dt; // z velocity
+        fx[6] = this->x[6]; // x acceleration
+        fx[7] = this->x[7]; // y acceleration
+        fx[8] = this->x[8]; // z acceleration
+        fx[9] = this->x[9] + this->x[12] * this->dt; // pitch
+        fx[10] = this->x[10] + this->x[13] * this->dt; // roll
+        fx[11] = this->x[11] + this->x[14] * this->dt; // yaw
+        fx[12] = this->x[12]; // pitch rate bias
+        fx[13] = this->x[13]; // roll rate bias
+        fx[14] = this->x[14]; // yaw rate bias
+
+        // State transition Jacobian matrix
+        F[0][0] = 1;
+        F[0][3] = this->dt;
+        F[1][1] = 1;
+        F[1][4] = this->dt;
+        F[2][2] = 1;
+        F[2][5] = this->dt;
+        F[3][3] = 1;
+        F[3][6] = this->dt;
+        F[4][4] = 1;
+        F[4][7] = this->dt;
+        F[5][5] = 1;
+        F[5][8] = this->dt;
+        F[6][6] = 1;
+        F[7][7] = 1;
+        F[8][8] = 1;
+        F[9][9] = 1;
+        F[9][12] = this->dt;
+        F[10][10] = 1;
+        F[10][13] = this->dt;
+        F[11][11] = 1;
+        F[11][14] = this->dt;
+        F[12][12] = 1;
+        F[13][13] = 1;
+        F[14][14] = 1;
+
+        // Measurement function hx = Hx
+        hx[0] = this->z[0]; // sensor 1 distance measurement
+        hx[1] = this->z[1]; // sensor 2 distance measurement
+        hx[2] = this->z[2]; // sensor 3 distance measurement 
+      }
+};
+Filter ekf;
 void setup() {
   // put your setup code here, to run once:
   // Initialize ultrasonic sensor pins
@@ -36,7 +143,18 @@ void loop() {
   float d2_meas = readUltrasonic(trigPin2, echoPin2);
   float d3_meas = readUltrasonic(trigPin3, echoPin3); 
   getIMU();
-  
+  // Send these measurements to the EKF
+  double z[3] = {d1_meas, d2_meas, d3_meas};
+  ekf.step(z);
+  Serial.print(z[0]);
+  Serial.print(" ");
+  Serial.print(z[1]);
+  Serial.print(" ");
+  Serial.print(z[2]);
+  Serial.print(" ");
+  Serial.print(ekf.getX(0));
+  Serial.print(" ");
+  Serial.println(ekf.getX(1));
   // send the sensor data over i2c as an array of bytes
   uint8_t sensor_data[] = {static_cast<uint8_t>(d1_meas), static_cast<uint8_t>(d2_meas), static_cast<uint8_t>(d3_meas)};
   Wire.beginTransmission(8); // transmit to device with address 8
@@ -50,31 +168,28 @@ void getIMU(){
     myICM.getAGMT();
 
     ICM_20948_I2C *sensor = &myICM;
-    float x = sensor->accX();
-    float y = sensor->accY();
-    float z = sensor->accZ();
-    float gx = sensor->gyrX();
-    float gy = sensor->gyrY();
-    float gz = sensor->gyrZ();       
+    x = sensor->accX();
+    y = sensor->accY();
+    z = sensor->accZ();
+    gx = sensor->gyrX();
+    gy = sensor->gyrY();
+    gz = sensor->gyrZ();       
     printData(x, y, z, gx, gy, gz);
   } 
 }
 void printData(float x, float y, float z, float gx, float gy, float gz){
-  // Serial.print("Ax: ");
-  //   Serial.println(x);
-  //   Serial.print("Ay: ");
-  //   Serial.println(y);
-  //   Serial.print("Az: ");
-  //   Serial.println(z);
+  Serial.print("Ax: ");
+    Serial.println(x);
+    Serial.print("Ay: ");
+    Serial.println(y);
+    Serial.print("Az: ");
+    Serial.println(z);
     Serial.print("Gx: ");
     Serial.println(gx);
     Serial.print("Gy: ");
     Serial.println(gy);
     Serial.print("Gz: ");
-    Serial.println(gz);
-    Serial.println("");
-    delay(3000);
-     
+    Serial.println(gz); 
 }
 void printSensorData(float sensorData){
   Serial.print("Sensor: ");
